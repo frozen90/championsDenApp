3f2998e2aa2405c2495a554d620c1ad3d1900180
frozen90 <pych60@gmail.com>
2020-01-01 21:33:51 +0000

    finding summoner by name #stage one

--- app/championsDen/views.py
+++ app/championsDen/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 import requests
 # Create your views here.
 
-API_KEY = 'RGAPI-38c91693-1ece-40fa-ac67-0df42a1c13f7'
+API_KEY = 'RGAPI-5ccb450e-29d5-4696-b60d-481a4047e541'
 
 
 def landingpage(request):
@@ -15,9 +15,26 @@ def landingpage(request):
 
 
 def getSummoner(request):
-    response = requests.get('https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/amfetaminator200?api_key=' + API_KEY)
-    summonerData = response.json()
-    print(summonerData)
-    level = summonerData['summonerLevel']
-    return HttpResponse(level
-    )
+
+    if request.POST:
+        username = request.POST.get('name_field')
+        response = requests.get("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + username + "?api_key=" + API_KEY)
+        summonerData = response.json()
+        try:
+
+            summoner = {
+            'id': summonerData['id'],
+            'accountID': summonerData['accountId'],
+            'puuid': summonerData['puuid'],
+            'name': summonerData['name'],
+            'profileIcon': summonerData['profileIconId'],
+            'revisionDate': summonerData['revisionDate'],
+            'summonerLevel': summonerData['summonerLevel']}
+            return render(request, 'summoner.html',{'summoner': summoner})
+
+        except: 
+            data = summonerData['status']
+            errormessage = data['message']
+            return render(request, 'summoner.html',{'errormessage': errormessage})
+
+    return render(request, 'summoner.html')
