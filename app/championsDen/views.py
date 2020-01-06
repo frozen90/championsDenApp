d49c5f0234bc4dae6a2737f68e047a3d73b8ede3
frozen90 <pych60@gmail.com>
2020-01-06 19:46:34 +0000

    Summoner view added last matches

--- app/championsDen/views.py
+++ app/championsDen/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 import requests
 # Create your views here.
 
-API_KEY = 'RGAPI-5ccb450e-29d5-4696-b60d-481a4047e541'
-
+API_KEY = 'RGAPI-ad7ca1bd-3af7-4f48-aa27-fd670a51d501'
+REGIONS = {'BR1','EUN1','EUW1','JP1','KR','LA1','LA2','NA1','OC1','TR1','RU'}
 
 def landingpage(request):
 
@@ -18,8 +18,12 @@ def getSummoner(request):
 
     if request.POST:
         username = request.POST.get('name_field')
-        response = requests.get("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + username + "?api_key=" + API_KEY)
+        region = request.POST.get('region')
+        response = requests.get("https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + username + "?api_key=" + API_KEY)
         summonerData = response.json()
+        matches = requests.get("https://" + region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + summonerData['accountId'] + "?api_key=" + API_KEY)
+        matchesData = matches.json()
+
         try:
 
             summoner = {
@@ -30,11 +34,11 @@ def getSummoner(request):
             'profileIcon': summonerData['profileIconId'],
             'revisionDate': summonerData['revisionDate'],
             'summonerLevel': summonerData['summonerLevel']}
-            return render(request, 'summoner.html',{'summoner': summoner})
+            return render(request, 'summoner.html',{'summoner': summoner, 'userRegion': region,'regions': REGIONS, 'matches' : matchesData})
 
-        except: 
+        except:
             data = summonerData['status']
             errormessage = data['message']
-            return render(request, 'summoner.html',{'errormessage': errormessage})
+            return render(request, 'summoner.html',{'errormessage': errormessage, 'regions': REGIONS})
 
-    return render(request, 'summoner.html')
+    return render(request, 'summoner.html', {'regions': REGIONS})
